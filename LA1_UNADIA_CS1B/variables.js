7d4d5791480d00f6463f778ffd394d985be06949
Unadia16 <unadiaaarumexander@gmail.com>
2025-03-05 07:55:42 +0800

    Add files via upload

---
+++ LA1_UNADIA_CS1B/variables.js
@@ -0,0 +1,12 @@
+let name = "Aarume Xander Unadia"; // Create variable name (name) & store your name.
+let whatDoYouWannaBecomeInYourLife = "programmer"; // Create variable name (whatDoYouWannaBecomeInYourLife) & store "programmer"
+let gender = "Male"; // Create variable name (gender) & store your gender.
+let facebookAcct = "Aarume Xander Unadia"; // Create variable name (facebookAcct) & store your Facebook Name.
+
+// Finally log all variables to the console using code runner in vscode.
+console.log('Name:'+name)
+console.log('Dream:'+whatDoYouWannaBecomeInYourLife)
+console.log('Gender:'+gender)
+console.log('Facebook Account:'+facebookAcct)
+
+// Submit answers in your github repository and link it to your google classroom.
