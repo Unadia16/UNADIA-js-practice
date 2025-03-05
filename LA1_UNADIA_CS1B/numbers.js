ad497876f4225cf41432b323495f20c6d891a4af
Unadia16 <unadiaaarumexander@gmail.com>
2025-03-05 22:49:54 +0800

    Add files via upload

---
+++ LA1_UNADIA_CS1B/numbers.js
@@ -0,0 +1,20 @@
+//UNADIA, AARUME XANDER BSCS-1B
+let firstFavNum = 16; //Create Variable name (firstFavNum) & store your favorite number.
+let secondFavNum = 24; //Create Variable name (secondFavNum) & store your second fav number.
+
+let sum = firstFavNum + secondFavNum; //Add (firstFavNum & secondFavNumber).
+let diff = firstFavNum - secondFavNum; //Subtract (firstFavNum & secondFavNumber).
+let prod = firstFavNum * secondFavNum; //Multiply (firstFavNum & secondFavNumber).
+let div = firstFavNum / secondFavNum; //Divided (firstFavNum & secondFavNumber).
+let mod = firstFavNum % secondFavNum; //Check (firstFavNum Mod secondFavNumber).
+let expo = firstFavNum ** secondFavNum; //Check the power of (firstFavNum) raise to secondFavNun.
+
+//Finally log all variables to the console using code runner in vscode.
+console.log("Addition:" + sum);
+console.log("Subtraction:" + diff);
+console.log("Multiplication:" + prod);
+console.log("Division:" + div);
+console.log("Modulo:" + mod);
+console.log("Exponentiation:" + expo);
+
+//Submit answers in your github repository and link it to your google classroom.
