13a8e9206ddac6b89bc759b29b2b0557d6509d45
Unadia16 <unadiaaarumexander@gmail.com>
2025-03-15 23:24:59 +0800

    Add files via upload

---
+++ LA1_UNADIA_CS1B/booleans.js
@@ -0,0 +1,11 @@
+//UNADIA, AARUME XANDER BSCS-1B
+let isJsProgrammingLanguage = true;//Create Variable name (isJsProgrammingLanguage) & store true as a value.
+let isJsHard = false;//Create variable name (isJsHard) & store false as a value.
+let favNumb = 16;//Create variable name (favNumb) & store your favorite number inside.
+
+favNumb = undefined;//Now Add favNumber with the value of undefined.
+
+//Finally log all variables to the console using code runner in vscode.
+console.log('Is JS a programming language?',isJsProgrammingLanguage);
+console.log('Is JS Hard?',isJsHard);
+console.log('My favorite number is',favNumb);
