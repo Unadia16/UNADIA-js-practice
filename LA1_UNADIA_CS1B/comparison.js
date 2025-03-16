547b90e5018573c8ffe2c89dc89751a17c0b2fa5
Unadia16 <unadiaaarumexander@gmail.com>
2025-03-16 09:05:44 +0800

    Add files via upload

---
+++ LA1_UNADIA_CS1B/comparison.js
@@ -0,0 +1,22 @@
+//UNADIA, AARUME XANDER BSCS-1B
+let firstFavNumb = 16;//Create variable name (firstFavNumb) & store your favorite number.
+let secondFavNumb = 24;//Create variable name (secondFavNumb) & store second fav Number.
+
+let greater_than = firstFavNumb > secondFavNumb;//Check (firstFavNumb is greater then secondFavNumb).
+let less_than = firstFavNumb < secondFavNumb;//Check (firstFavNumb is less then secondFavNumb).
+let greater_than_or_equal_to = firstFavNumb >= secondFavNumb;//Check (firstFavNumb is greater then & equal to secondFavNumb).
+let less_than_or_equal_to = firstFavNumb <= secondFavNumb;//Check (firstFavNumb is less then & equal to secondFavNumb).
+let strict_equality  = firstFavNumb === secondFavNumb;//Check (firstFavNumb is equal to secondFavNumb) using strict equality operator.
+let loose_equality = firstFavNumb == secondFavNumb;//Check (firstFavNumb is equal to secondFavNumb) using loose equality operator.
+let strict_inequality  = firstFavNumb !== secondFavNumb;//Check (firstFavNumb is not equal to secondFavNumb) using strict non-equality operator.
+let loose_inequality = firstFavNumb != secondFavNumb;//Check is (firstFavNumb is not equal to secondFavNumb) using loose non-equality operator.
+
+//Finally log all variables to the console using code runner in vscode.
+console.log('Greater than:',greater_than);
+console.log('Less than:',less_than);
+console.log('Greater than or equal to:',greater_than_or_equal_to);
+console.log('Less than or equal to:',less_than_or_equal_to);
+console.log('Strict equality (===):',strict_equality);
+console.log('Loose equality (==):',loose_equality);
+console.log('Strict inequality (!==):',strict_inequality);
+console.log('Loose inequality (!=):',loose_inequality);
